e2e0bcd8d35bec62f5cc8599d348e9971145cf9e
Matthew Howat <32020464+MJHowatCreations@users.noreply.github.com>
2018-09-05 09:01:29 -0600

    commenting apps goals

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Enter a location for weather information
+
+
+
+//View current climate information for the location
+
+
+//yahoo api example "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="london, on")" we receive a json object
+//current temp object is "item"
+
+
+
+//View the 5 day forecast for the location
+
+//json object contains forecast list with 5 values for the next 5 days. key values pairs are: "code" "date" "day" "high" "low" "text"
+
+
