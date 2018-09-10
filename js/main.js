babd9e78d7a5367331a0efd8fd4403a31f090444
Matthew Howat <32020464+MJHowatCreations@users.noreply.github.com>
2018-09-10 09:50:54 -0600

    second day ajax request received

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 // Enter a location for weather information
 
 //need an event listener that listens to the click of the button and grabs the id="location" text value
-document.querySelector('[type="submit"]').addEventListener('click', function(evt){
-    var locationForm = document.querySelector('[name="location"]');
 
-    document.querySelector('h1').innerHTML = locationForm.value; 
+$('.frm.weather').on('submit', function(evt) {
+    const   location = $(evt.target).find('[name=location]').val(),
+            query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + location + "') and u = 'c'",
+            endpoint = "https://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json&env=store://datatables.org/alltableswithkeys";
 
 
-    
+    $.getJSON(endpoint, function (data) {
+        console.log(data);
+
+
+    });
+
+
     evt.preventDefault(); 
-})
+
+
+});
+
 
 
 //View current climate information for the location
 
 
-//yahoo api example "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="london, on")" we receive a json object
+//yahoo api example "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="london, on") and u = 'c'" we receive a json object
 //current temp object is "item"
 
 
