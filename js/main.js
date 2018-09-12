d131275393959f669ccd0c3a74d86682a81b4b44
Matthew Howat <32020464+MJHowatCreations@users.noreply.github.com>
2018-09-12 09:51:10 -0600

    weather app completed

--- js/main.js
+++ js/main.js
@@ -2,24 +2,42 @@
 
 //need an event listener that listens to the click of the button and grabs the id="location" text value
 
-$('.frm.weather').on('submit', function(evt) {
-    const   location = $(evt.target).find('[name=location]').val(),
-            query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + location + "') and u = 'c'",
-            endpoint = "https://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json&env=store://datatables.org/alltableswithkeys";
+(function($) {
+    'use strict';
 
+    function displayWeather(data, $el) {
+        var $loc = $($el).find('.details>.location'),
+            $date = $($el).find('.details>.date'),
+            $conditions = $($el).find('.details>.conditions'),
+            $currentTemp = $($el).find('.details>.temp'),
+            $sunrise = $($el).find('.details>.sunrise'),
+            $sunset = $($el).find('.details>.sunset');
 
-    $.getJSON(endpoint, function (data) {
-        console.log(data);
+            $loc.text(data.location.city + ', ' + data.location.region);
+            $date.text(data.lastBuildDate);
+            $conditions.text(data.item.condition.text);
+            $currentTemp.text(data.item.condition.temp);
+            $sunrise.text(data.astronomy.sunrise);
+            $sunset.text(data.astronomy.sunset);
 
+    }
 
+    $('.frm.weather').on('submit', function(evt) {
+        const   location = $(evt.target).find('[name=location]').val(),
+                query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + location + "') and u = 'c'",
+                endpoint = "https://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json&env=store://datatables.org/alltableswithkeys";
+        
+        
+        evt.preventDefault(); 
+
+        $.getJSON(endpoint, function (data) {
+            data = data.query.results.channel;
+            displayWeather(data, $('.weather-display'));
+        });
     });
 
 
-    evt.preventDefault(); 
-
-
-});
-
+}(jQuery));
 
 
 //View current climate information for the location
